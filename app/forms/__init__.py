3dfb8312b17253fbf7e246ce0972805e026dfdef
Jon Moore <jonevanmoore@yahoo.com>
2022-08-01 19:19:27 -0700

    book form init

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,3 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+from .books import CreateAuthorForm, CreateBookForm, CreateGenreForm, CreateReviewForm, CreateStatusForm, EditBookForm, EditReviewForm, EditStatusForm
