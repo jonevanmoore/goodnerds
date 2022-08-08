7d8b14e467b6ce93411d1d123a200d24528b4434
Jon Moore <jonevanmoore@yahoo.com>
2022-08-08 08:54:47 -0700

    book genre model

---
+++ app/models/books/book_genre.py
@@ -0,0 +1,13 @@
+from .db import db
+
+class BookGenre(db.Model):
+    __tablename__ = 'book_genres'
+
+    id = db.Column(db.Integer, primary_key=True)
+    genre = db.Column(db.String)
+
+def to_dict(self):
+    return {
+    'id': self.id,
+    'genre': self.genre
+    }
